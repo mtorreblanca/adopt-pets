864a4ab561fd9b7026fe34257b3ae3a8d687397d
marcelo.torreblanca@segurosarca.com <marcelo.torreblanca@segurosarca.com>
2020-08-31 14:35:13 -0500

    Added Sass

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import BoostrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/index.sass'
 
 import App from './App.vue'
 import router from './router'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home-view-container">
     <h1>Adopt a new best friend.</h1>
     {{ animalsCount }} {{ getAllCats.length }}
     <button @click="togglePetForm" type="button" class="btn btn-primary">Add New Pet</button>
